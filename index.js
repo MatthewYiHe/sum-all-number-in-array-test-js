2b34d7f2f276aa43d475e23104ef669f600e6352
MatthewYiHe <matthewhe1@hotmail.com>
2019-05-29 19:34:40 +0000

    finished exercise

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+// let sum = 0
 function sumItems(array) {
+  let sum = 0;
   // Sum all the numbers in the array
+  array.forEach((item) => {
+    if (Array.isArray(item)){
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+  });
+  return sum;
 }
 
 module.exports = sumItems;
+
+// console.log(sumItems([[1, 2, [[3], 4]], 5, []]))
